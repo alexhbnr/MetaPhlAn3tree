74e40010fbe8d120a9dc795225a6828a5c1f08fe
Alex Huebner <alexhbnr@gmail.com>
2020-04-28 07:22:31 +0200

    Add option to subset number of taxa by name and bugfixes

--- src/species.py
+++ src/species.py
@@ -41,7 +41,7 @@ class Species:
         self.db_version = db_version
         self.db_dir = db_dir
         self.pkl_fn = f'{self.db_dir}/mpa_v{self.db_version}_CHOCOPhlAn_201901.pkl'
-        
+
     def extract_strains(self):
         """Extract genome IDs of strains present in database file."""
         def parse_strain_info(strainname, strainnumbers):
@@ -49,18 +49,18 @@ class Species:
             gca_id = extract_gcaid.search(strainname).group(1)
             taxid = int(strainnumbers[0].split("|")[-1])
             taxonomy = pd.DataFrame([split_rank.search(t).groups()
-                                        for t in extract_taxonomy.search(strainname). \
-                                        group(1).split("|")], columns=["abbr", "unit"])
+                                     for t in extract_taxonomy.search(strainname).
+                                     group(1).split("|")], columns=["abbr", "unit"])
             taxonomy['rank'] = taxonomy['abbr'].map(tax_abbr)
             return taxonomy.drop(['abbr'], axis=1).set_index('rank').T \
-                .assign(GCAid = gca_id) \
-                .assign(taxid = taxid) \
-                .assign(genomelength = strainnumbers[1]) \
+                .assign(GCAid=gca_id) \
+                .assign(taxid=taxid) \
+                .assign(genomelength=strainnumbers[1]) \
                 [['GCAid', 'taxid', 'genomelength'] + list(tax_abbr.values())] \
                 .reset_index(drop=True)
 
         with bz2.open(self.pkl_fn, "rb") as bzfile:
-             strains = pickle.load(bzfile)['taxonomy']
+            strains = pickle.load(bzfile)['taxonomy']
         genome_df = [parse_strain_info(name, numbers)
                      for name, numbers in strains.items()]
         self.genomes = pd.concat(genome_df)
@@ -72,7 +72,7 @@ class Species:
                                        'taxid', 'species_taxid', 'assembly_level',
                                        'release_type', 'genome_rep', 'ftp_path']) \
             .rename({'taxid': 'strain_taxid'}, axis=1)
-        genbank['GCAid'] = genbank['# assembly_accession'].str.replace("\.[0-9]+$", "")
+        genbank['GCAid'] = genbank['# assembly_accession'].str.replace(r"\.[0-9]+$", "")
         self.genomes = self.genomes.merge(genbank, how="left", on=['GCAid'])
 
     def get_missing_information(self):
@@ -82,7 +82,7 @@ class Species:
                                         'GCAid'].tolist()
         if len(missing_gcas) > 0:
             print(f"\t\t{len(missing_gcas)} genomes have missing information. Search "
-                "on NCBI Assembly for them.", file=sys.stderr)
+                  "on NCBI Assembly for them.", file=sys.stderr)
             # Retrieve for each missing entry from NCBI Assembly
             gca_entries = []
             for gcaid in missing_gcas:
@@ -93,17 +93,17 @@ class Species:
                 gca_entries.append([esummary_record['DocumentSummarySet'] \
                                     ['DocumentSummary'][0][f]
                                     for f in ['AssemblyAccession', 'RefSeq_category',
-                                            'Taxid','SpeciesTaxid', 'AssemblyStatus',
-                                            'ReleaseType',
-                                            'PartialGenomeRepresentation',
-                                            'FtpPath_GenBank']])
+                                              'Taxid', 'SpeciesTaxid', 'AssemblyStatus',
+                                              'ReleaseType',
+                                              'PartialGenomeRepresentation',
+                                              'FtpPath_GenBank']])
             # Generate Pandas DataFrame for merging
             gca_dfs = pd.DataFrame(gca_entries, columns=['# assembly_accession',
-                                                        'refseq_category',
-                                                        'strain_taxid', 'species_taxid',
-                                                        'assembly_level', 'release_type',
-                                                        'genome_rep', 'ftp_path']) \
-                    .assign(GCAid = missing_gcas)
+                                                         'refseq_category',
+                                                         'strain_taxid', 'species_taxid',
+                                                         'assembly_level', 'release_type',
+                                                         'genome_rep', 'ftp_path']) \
+                    .assign(GCAid=missing_gcas)
             gca_dfs['genome_rep'] = gca_dfs['genome_rep'].replace({'false': 'Full',
                                                                    'true': 'Partial'})
             # Replace missing values
@@ -111,44 +111,70 @@ class Species:
                                                self.genomes.columns.tolist()[:10]].copy()
             missing_genomes = missing_genomes.merge(gca_dfs, how="left", on=['GCAid'])
             self.genomes = pd.concat([self.genomes.loc[~self.genomes['assembly_level'].isnull()],
-                                            missing_genomes])
+                                      missing_genomes])
         else:
             print("\t\tNo genomes have missing information. Continue.", file=sys.stderr)
 
+    def subset_taxa(self, taxalist):
+        """Subset representative genomes to selected ones for downloading."""
+        if len(taxalist) > 0:
+            self.genomes_set = self.genomes.loc[self.genomes['species'].isin(taxalist)]
+            skipped_taxa = [t for t in taxalist
+                            if t not in self.genomes_set['species'].tolist()]
+            print(f"\tFrom {len(taxalist)} species names in the specified list "
+                  f"of taxa, {self.genomes_set['species'].unique().shape[0]} were "
+                  f"found in MetaPhlAn database. The following species names "
+                  f"are not present in the database: {', '.join(skipped_taxa)}.",
+                  file=sys.stderr)
+            if self.genomes_set.shape[0] > 0:
+                print(f"\tContinue with remaining {len(taxalist) - len(skipped_taxa)}"
+                      " species.", file=sys.stderr)
+            else:
+                print("\tNo species left. Select species present in the "
+                      "database.", file=sys.stderr)
+                sys.exit(1)
+        else:
+            self.genomes_set = self.genomes
+
     def determine_representative_genomes(self):
         """Determine the representative genome by evaluating assembly info."""
-        assembly_stats = self.genomes[['GCAid', 'taxid', 'assembly_level',
-                                       'release_type', 'genome_rep']].copy()
+        assembly_stats = self.genomes_set[['GCAid', 'taxid', 'assembly_level',
+                                           'release_type', 'genome_rep']].copy() \
+            .reset_index(drop=True)
 
         # Convert assembly columns into ranked categorial data types
         asslevel_type = CategoricalDtype(categories=['Complete Genome',
                                                      'Chromosome', 'Scaffold',
                                                      'Contig'], ordered=True)
         assembly_stats.loc[:, ('assembly_level')] = assembly_stats.loc[:, ('assembly_level')] \
-                .astype(asslevel_type) \
-                .cat.codes
+            .astype(asslevel_type) \
+            .cat.codes
         release_type = CategoricalDtype(categories=['Major', 'Minor'],
                                         ordered=True)
         assembly_stats.loc[:, ('release_type')] = assembly_stats.loc[:, ('release_type')] \
-                .astype(release_type) \
-                .cat.codes
+            .astype(release_type) \
+            .cat.codes
         genrep_type = CategoricalDtype(categories=['Full', 'Partial'],
                                        ordered=True)
         assembly_stats.loc[:, ('genome_rep')] = assembly_stats.loc[:, ('genome_rep')] \
-                .astype(genrep_type) \
-                .cat.codes
+            .astype(genrep_type) \
+            .cat.codes
         # Calculate overall rank
-        assembly_stats['rank'] = assembly_stats.loc[:, ~assembly_stats.columns \
-                .isin(['taxid', 'GCAid'])].sum(axis=1)
-        self.representative_genomes = assembly_stats.iloc[assembly_stats \
-                                                          .groupby(['taxid']) \
+        assembly_stats['rank'] = assembly_stats.loc[:, ~assembly_stats.columns
+                                                    .isin(['taxid', 'GCAid'])].sum(axis=1)
+        # Subset genome with lowest rank per taxid and sample randomly multiple
+        # with equal rank
+        self.representative_genomes = assembly_stats.iloc[assembly_stats
+                                                          .groupby(['taxid'])
                                                           ['rank'].idxmin()] \
-                                                          ['GCAid'].unique().tolist()
+                                                    .groupby(['taxid']) \
+                                                    .apply(lambda d: d.sample(1))['GCAid'] \
+                                                    .unique().tolist()
 
     def write_url_list(self, outfn):
         """Write list of links to genomes for representative genomes to file."""
         def generate_url(ftppath):
-            return f'{ftppath}/{os.path.basename(ftppath)}_genomic.fna.gz' 
+            return f'{ftppath}/{os.path.basename(ftppath)}_genomic.fna.gz'
 
         link_df = self.genomes.loc[self.genomes['GCAid'].isin(self.representative_genomes),
                                    ('GCAid', 'ftp_path')].drop_duplicates().copy()
@@ -165,7 +191,7 @@ class Species:
                 esummary_record = Entrez.read(esummary_handle)
                 ftp_entries.append(esummary_record['DocumentSummarySet'] \
                                    ['DocumentSummary'][0]['FtpPath_RefSeq'])
-        link_df.loc[link_df['ftp_path'] == "", 'ftp_path'] = ftp_entries
+            link_df.loc[link_df['ftp_path'] == "", 'ftp_path'] = ftp_entries
         print(f"\t\tSkipping {link_df.loc[link_df['ftp_path'] == ''].shape[0]} "
               f"genomes ({', '.join(link_df.loc[link_df['ftp_path'] == '', 'GCAid'].tolist())})"
               " from analysis because of no FTP link information.", file=sys.stderr)
